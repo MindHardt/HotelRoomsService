db499a756e796f564b67a72457f9235b82d0f024
geer.max0404 <geer.max0404@gmail.com>
2023-06-22 19:37:15 +0500

    Added route for changing room status

--- Core.Requests/PutOrderRequest.cs
+++ Core.Requests/PutOrderRequest.cs
@@ -1,4 +1,6 @@
-﻿using Core.Responses;
+﻿using System.Text.Json.Serialization;
+using Core.Entities;
+using Core.Responses;
 using MediatR;
 
 namespace Core.Requests;
@@ -8,4 +10,6 @@ public class PutOrderRequest : IRequest<PutOrderResponse>
     public required float HotelLatitude { get; set; } 
     public required float HotelLongitude { get; set; } 
     public required int RoomNumber { get; set; }
+    public RoomCleanState State { get; set; }
+    public required bool IsCleaningRequested { get; set; }
 }
